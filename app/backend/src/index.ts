5396d5fc8273910de176ceb97ba5c6c612969b64
Vincent <vincent.devilers@gmail.com>
2025-10-18 15:57:00 +0200

    fix all backend error and warning

--- app/backend/src/index.ts
+++ app/backend/src/index.ts
@@ -6,27 +6,27 @@ import { router } from "./router";
 const handler = new RPCHandler(router);
 
 const app = new Elysia()
-  .use(
-    cors({
-      origin: ["http://localhost:3001"], // Your frontend URL
-      credentials: true,
-    }),
-  )
-  .get("/", "hello world")
-  .all(
-    "/rpc*",
-    async ({ request }: { request: Request }) => {
-      const { response } = await handler.handle(request, {
-        prefix: "/rpc",
-      });
-      return response ?? new Response("Not Found", { status: 404 });
-    },
-    {
-      parse: "none",
-    },
-  )
-  .listen(3000);
+	.use(
+		cors({
+			origin: ["http://localhost:3001"], // Your frontend URL
+			credentials: true,
+		}),
+	)
+	.get("/", "hello world")
+	.all(
+		"/rpc*",
+		async ({ request }: { request: Request }) => {
+			const { response } = await handler.handle(request, {
+				prefix: "/rpc",
+			});
+			return response ?? new Response("Not Found", { status: 404 });
+		},
+		{
+			parse: "none",
+		},
+	)
+	.listen(3000);
 
 console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`,
+	`🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`,
 );
--- app/backend/src/router/index.ts
+++ app/backend/src/router/index.ts
@@ -2,10 +2,10 @@ import { os } from "@orpc/server";
 import { addTodo, listTodos } from "./todos";
 
 export const router = os.router({
-  todo: {
-    list: listTodos,
-    add: addTodo,
-  },
+	todo: {
+		list: listTodos,
+		add: addTodo,
+	},
 });
 
 export type Router = typeof router;
