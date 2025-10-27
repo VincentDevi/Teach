6b48f831e571325fae5a48c9c348e2e3febb6286
Devilers Vincent <53350702+VincentDevi@users.noreply.github.com>
2025-10-27 11:51:17 +0100

    Revert "Vincentdevilers/pip 9 create clientdetails component"

--- app/web/src/components/ui/details/detailsSubTitle.tsx
+++
@@ -1,9 +0,0 @@
-function DetailsSubTitle({ children }: { children: string }) {
-  return (
-    <h3 className="text-sm font-semibold leading-none tracking-tight">
-      {children}
-    </h3>
-  );
-}
-
-export { DetailsSubTitle };
